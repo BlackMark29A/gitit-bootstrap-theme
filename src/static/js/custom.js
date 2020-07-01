552c6811393d7f93de1784795221b281164598e9
BlackMark <me@blackmark.me>
2020-07-01 22:11:06 +0200

    Add search-and-replace basic localization for german

--- src/static/js/custom.js
+++ src/static/js/custom.js
@@ -7,6 +7,42 @@ $(document).ready(function() {
     .filter(function() {
       return this.hostname && this.hostname !== location.hostname
     })
-    .append('<i class="external-link fa fa-external-link-alt small"></i>')
+    .append('<i class="external-link fa fa-external-link-alt small"></i>');
 
-})
+  /* Search and replace localization */
+  var prefLang = getPreferredLanguage();
+  var lang = getParameterValue("lang");
+
+  if (!lang)
+    lang = prefLang;
+  if (lang) String.locale = lang;
+
+  localizeAllNodes();
+});
+
+/* Localization helper functions */
+
+function getParameterValue(parameter) {
+  parameter = parameter.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regexS = "[\\?&]" + parameter + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(window.location.href);
+  if (results == null)
+    return "";
+  else
+    return results[1];
+}
+
+function getPreferredLanguage() {
+  return navigator.languages
+    ? navigator.languages[0]
+    : (navigator.language || navigator.userLanguage);
+}
+
+function localizeAllNodes() {
+  var node;
+  var walker = document.createTreeWalker(document.documentElement, NodeFilter.SHOW_TEXT, null, false);
+  while (node = walker.nextNode()) {
+    node.textContent = node.textContent.toLocaleString();
+  }
+}
